98da764132183f8e09cab1a1fd0b047100eed1b3
Yadav-16 <yadav.16aku@gmail.com>
2023-09-16 00:05:04 -0500

    Commit 1

---
+++ Profile/src/userProfile.java
@@ -0,0 +1,18 @@
+
+public class userProfile {
+String firstName;
+String lastName;
+String dateofBirth;
+String emailAddress;
+long phoneNumber;
+String userID;
+String Password;
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
